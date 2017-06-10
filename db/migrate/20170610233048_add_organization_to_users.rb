6c046825666bdb2a2c2884e35a2925aaf44fd64f
EricLScace <mail@ericscace.com>
2017-06-10 19:33:21 -0400

    Issue #51, #53
    Owners information was added to Users table rather than create another
    table with a belongs_to/has_one relationship. Will update documentation
    later if this works; created issue #53 for this.

---
+++ db/migrate/20170610233048_add_organization_to_users.rb
@@ -0,0 +1,5 @@
+class AddOrganizationToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :organization, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170610222420) do
+ActiveRecord::Schema.define(version: 20170610233048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 20170610222420) do
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.text     "name"
+    t.text     "organization"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end

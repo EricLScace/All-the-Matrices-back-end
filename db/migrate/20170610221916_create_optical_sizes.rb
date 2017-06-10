4091d07327cb76a50d44b99eb52fac814391aff7
EricLScace <mail@ericscace.com>
2017-06-10 18:32:06 -0400

    Issue #46 fixed use of scale: in optical size
    A few stray characters entered in the last commit on this table.

--- db/migrate/20170610221916_create_optical_sizes.rb
+++ db/migrate/20170610221916_create_optical_sizes.rb
@@ -1,7 +1,7 @@
 class CreateOpticalSizes < ActiveRecord::Migration[5.0]
   def change
     create_table :optical_sizes do |t|
-      t.decimal :points, scale: git pus3
+      t.decimal :points, scale: 3
 
       t.timestamps
     end

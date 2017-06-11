6e85e16b7e047e77c66549f3eeb37c64011bbb55
EricLScace <mail@ericscace.com>
2017-06-11 15:02:10 -0400

    Closes #63
    Sign-up working on dev & Heroku.

--- scripts/sign-up.sh
+++ scripts/sign-up.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+EMAIL="eric@scace.org"
+PASSWORD="k3na"
+API="https://allthemats.herokuapp.com/"
+# API="${API_ORIGIN:-http://localhost:4741}"
 
-API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/sign-up"
 curl "${API}${URL_PATH}" \
   --include \

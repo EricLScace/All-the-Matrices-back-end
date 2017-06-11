b2c6184410d7c13baae86a4588947ea1dd7f2ae0
EricLScace <mail@ericscace.com>
2017-06-10 21:14:56 -0400

    Issue #50
    It seems that the 3 typefaces on the matrix must be separate tables,
    each with a belongs_to relationship to Typefaces.

--- db/migrate/20170611001045_create_matrices.rb
+++ db/migrate/20170611001045_create_matrices.rb
@@ -6,9 +6,6 @@ class CreateMatrices < ActiveRecord::Migration[5.0]
       t.belongs_to :manufacturer, index: true
       t.decimal :code_prefix, scale: 2, precision: 6
       t.integer :code_suffix
-      # t.index :typeface, name: typeface_normal_id
-      # t.index :typeface, name: typeface_auxiliary1_id
-      # t.index :typeface, name: typeface_auxiliary2_id
       t.timestamps
     end
   end

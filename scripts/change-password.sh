6e56184874affbb782023d8edf28d3e1010804e2
EricLScace <mail@ericscace.com>
2017-06-11 15:42:54 -0400

    Closes #64 #65
    curl scripts for AuthN work in dev & Heroku.

--- scripts/change-password.sh
+++ scripts/change-password.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+TOKEN="BAhJIiU3MjE1ZmExYWI1NDFlOWIzN2NkODY3YTE0NmNlNjljZAY6BkVG--95b9f8fe54764abe3b7c427fa3202a6ee02f7cae"
+ID="1"
+OLDPW="k3na"
+NEWPW="k3nak3na"
 
-API="${API_ORIGIN:-http://localhost:4741}"
+API="https://allthemats.herokuapp.com/"
+# API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/change-password"
 curl "${API}${URL_PATH}/${ID}" \
   --include \
